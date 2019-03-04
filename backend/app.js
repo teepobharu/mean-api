946c30b8fb35b48054f03d1dc42f340385c8ea84
teepobH <teepob.haru@gmail.com>
2019-03-04 18:19:32 +0700

    api mean

---
+++ backend/app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const path = require("path");
+
+const postsRoutes = require("./routes/posts")
+const userRoutes = require("./routes/user")
+
+const app = express();
+
+mongoose
+    .connect(
+        "mongodb+srv://admin:admin@cluster0-uicrg.mongodb.net/node-angular?retryWrites=true", { useNewUrlParser: true }
+    )
+    .then(() => {
+        console.log("Connected to database!");
+    })
+    .catch((err) => {
+        console.log("Connection failed!", err);
+    });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/images", express.static(path.join(__dirname, "backend/images")));
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+    );
+    next();
+});
+
+app.use("/api/posts", postsRoutes);
+app.use("/api/user", userRoutes);
+
+module.exports = app;
